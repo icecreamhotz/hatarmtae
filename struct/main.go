6356550f611e16c7259a58ee4b63e9f0d00333ed
Anucha Phudtapranee <anucha@igeargeek.com>
2021-05-18 18:08:14 +0700

    [Edited]haahaha

--- struct/main.go
+++ struct/main.go
@@ -5,12 +5,23 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contract  struct {
+		email   string
+		zipcode string
+	}
 }
 
 func main() {
 	p := person{
 		"Anucha",
 		"P",
+		struct {
+			email   string
+			zipcode string
+		}{
+			email:   "admin@hotmail.com",
+			zipcode: "001",
+		},
 	}
 	fmt.Printf("%v", p.print())
 }
